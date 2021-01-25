e8c8441ddf2dc88e3bc099fe2c1a69c440d66c05
eugene-afanasyev <whoaeugene@gmail.com>
2021-01-25 21:51:02 +0900

    fixed connection points

--- src/main/java/sample/Connection.java
+++ src/main/java/sample/Connection.java
@@ -28,13 +28,13 @@ public class Connection extends CubicCurve {
     }
 
     public void refreshCurvePos() {
-        setStartX(inputNode.getInputPane().localToScene(0,0).getX() - 2);
+        setStartX(inputNode.getInputPane().localToScene(0,0).getX() + 2);
         setStartY(inputNode.getNodeInner().localToParent(0,0).getY() + inputNode.getNodeInner().getHeight() / 2.0);
-        setControlX1(inputNode.getInputPane().localToScene(0,0).getX());
-        setControlY1(inputNode.getNodeInner().localToParent(0,0).getY());
-        setControlX2(outputNode.getNodeInner().localToParent(0,0).getX() - 20);
-        setControlY2(outputNode.getNodeInner().localToParent(0,0).getY());
-        setEndX(outputNode.getNodeInner().localToParent(0,0).getX() + 2);
+        setControlX1(inputNode.getInputPane().localToScene(0,0).getX() + 30);
+        setControlY1(inputNode.getNodeInner().localToParent(0,0).getY() + 50);
+        setControlX2(outputNode.getNodeInner().localToParent(0,0).getX() - 30);
+        setControlY2(outputNode.getNodeInner().localToParent(0,0).getY() + 50);
+        setEndX(outputNode.getNodeInner().localToParent(0,0).getX() + 12);
         setEndY(outputNode.getNodeInner().localToParent(0,0).getY() + inputNode.getNodeInner().getHeight() / 2.0);
     }
 
